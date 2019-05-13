0a3daa679c7133db2b4278ece133a0dc421356dc
ShaunKolich <employmeinc@gmail.com>
2019-05-13 19:36:50 -0400

    Started customizing the DOM created a Index.JS file

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//Logo
+let logo = document.getElementsByClassName('logo')[0];
+logo.textContent = 'Shaun Kolich';
+
+//Header information
+let headerInfo = document.getElementsByClassName('pro-name')[0];
+// console.log(headerInfo);
+headerInfo.textContent = 'Full Stack Web Developer';
+
+let headerBtn = document.getElementById('headerbtn');
+// console.log(headerBtn);
+headerBtn.textContent = 'Contact Me';
+
+//Middle Section
+
+
+    let middleSection = document.getElementsByClassName('block-heading cleafix')[0];
+middleSection.textContent = 'My Portfolio';
+middleSection.style.fontFamily = 'Milonga', 'cursive ';
+middleSection.style.fontWeight = '400';
+middleSection.style.fontSize = '55px';
+middleSection.style.wordSpacing = '5px';
+middleSection.style.color = 'grey';
